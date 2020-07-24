e8624947af12cd7d01ae7717a1faa8478d4e64e5
Vasiliy Zukanov <techyourchance@gmail.com>
2020-07-24 15:30:24 +0300

    Migration to Hilt: setup of Gradle dependencies, definition of @HiltAndroidApp and @AndroidEntryPoint's

--- app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
+++ app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
@@ -4,14 +4,11 @@ import android.app.Application
 import com.techyourchance.dagger2course.common.dependnecyinjection.app.AppComponent
 import com.techyourchance.dagger2course.common.dependnecyinjection.app.AppModule
 import com.techyourchance.dagger2course.common.dependnecyinjection.app.DaggerAppComponent
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
+@HiltAndroidApp
 class MyApplication: Application() {
-
-    public val appComponent: AppComponent by lazy {
-        DaggerAppComponent.builder()
-                .appModule(AppModule(this))
-                .build()
-    }
 
     override fun onCreate() {
         super.onCreate()
